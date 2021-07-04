802fc31936dad1dd4bd963bd225c76d7ea6b01e5
Nate Tyalor <taylonr@gmail.com>
2021-07-04 13:19:00 -0500

    Updating readme

--- promises.js
+++ promises.js
@@ -29,10 +29,9 @@ const loadPartialMetadata = () => {
 };
 
 const loadAllMetadata = () => {
-    let categories = axios.get("http://localhost:8888/itemCategories");
-    let statuses = axios.get("http://localhost:8888/orderStatuses");
-    let userTypes = axios.get("http://localhost:8888/userTypes");
     let addressTypes = axios.get("http://localhost:8888/addressTypes");
+    let categories = axios.get("http://localhost:8888/itemCategories");
+    let statuses = axios.get("http://localhost:8888/orderStatuses");    
 };
 
 module.exports = {
