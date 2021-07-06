a25e97f1ee7b113e2c736dbf1ac5466a61b154e4
Nate Tyalor <taylonr@gmail.com>
2021-07-06 17:49:03 -0500

    Better use cases for creating & resolving/rejecting a promise

--- created.promises.js
+++ created.promises.js
@@ -1,8 +1,16 @@
-const resolvedPromise = () => {};
-
-const rejectedPromise = () => {};
+const customPromise = (id, cb) => {    
+    new http.get(`http://localhost:8888/orderStatuses/${id}`, (res) => {
+        if(res.statusCode === 200){
+            res.on('data', (chunk) => {
+                cb(JSON.parse(chunk));
+            });
+        } else {
+            res.resume();
+            cb(res.statusCode);
+        }       
+    });
+};
 
 module.exports = {
-    rejectedPromise,
-    resolvedPromise,
+    customPromise,
 }
