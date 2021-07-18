2e64fd81da44740fa575f77a381c4725472c7bd4
Nate Tyalor <taylonr@gmail.com>
2021-07-18 11:26:58 -0500

    Updating starting URL

--- store/modules/orders.js
+++ store/modules/orders.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 module.exports = {
     loadOrders : (req, res) => {
         
-        let statuses = axios.get("http://localhost:3000/orderStatuses");
-        let users = axios.get("http://localhost:3000/users");
-        let orders = axios.get('http://localhost:3000/orders?userId=2');
-        let items = axios.get("http://localhost:3000/items");
-        let addresses = axios.get("http://localhost:3000/addresses");
+        let statuses = axios.get("http://localhost:8888/orderStatuses");
+        let users = axios.get("http://localhost:8888/users");
+        let orders = axios.get('http://localhost:8888/orders?userId=2');
+        let items = axios.get("http://localhost:8888/items");
+        let addresses = axios.get("http://localhost:8888/addresses");
     
         let metadata = {};
         
